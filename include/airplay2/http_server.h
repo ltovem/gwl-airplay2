a2bdaacfc079bee73b997a1201b268dce5331a9e
田高伟 <t@ltove.com>
2026-08-15 01:35:48 +0800

    feat: add minimal HTTP server API

---
+++ include/airplay2/http_server.h
@@ -0,0 +1,37 @@
+#pragma once
+
+#include <cstdint>
+#include <functional>
+#include <string>
+
+namespace gwl::airplay2 {
+
+struct HttpRequest {
+    std::string method;
+    std::string target;
+    std::string body;
+};
+
+struct HttpResponse {
+    int status = 200;
+    std::string content_type = "text/plain";
+    std::string body;
+};
+
+using HttpHandler = std::function<HttpResponse(const HttpRequest&)>;
+
+class HttpServer {
+public:
+    HttpServer();
+    ~HttpServer();
+
+    bool start(std::uint16_t port, HttpHandler handler);
+    void stop();
+    bool running() const noexcept;
+
+private:
+    class Impl;
+    Impl* impl_;
+};
+
+} // namespace gwl::airplay2
